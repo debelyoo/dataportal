1a01a268da93cea6003fc810838f64bf69a0ac67
debelyoo <jean.rossier@epfl.ch>
2013-10-14 15:01:00 +0200

    change /playproxy path to /portal (as prod) in JS

--- public/javascripts/map/config.js
+++ public/javascripts/map/config.js
@@ -1,6 +1,6 @@
 var Config = Backbone.Model.extend({
 	defaults: {
-		URL_PREFIX: "/playproxy",
+		URL_PREFIX: "/portal",
 		MAX_NB_DATA_POINTS_ON_MAP: 2000,
 		MAX_NB_DATA_POINTS_SINGLE_GRAPH: 2500,
 		ULM_DATE_CLASSNAME: 'ulmDate',
