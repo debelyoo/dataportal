6b847d1cddc77aecfd733e2d846c3b79c486ea98
debelyoo <jean.rossier@epfl.ch>
2013-11-15 12:54:46 +0100

    fix progress bar break due to bootstrap v3

--- public/javascripts/progress.js
+++ public/javascripts/progress.js
@@ -5,7 +5,7 @@ function getProgress(batchId, progressType, urlPrefix) {
         var prog = jsonData['progress'];
         var hint = jsonData['hint'];
         $("#progressPlaceholder").html(prog + "%");
-        $(".progress .bar").css({'width': prog + "%"});
+        $(".progress .progress-bar").css({'width': prog + "%"});
         $("#hintPlaceholder").html(hint);
         if (prog != 100) {
             // update progress every 1 sec
